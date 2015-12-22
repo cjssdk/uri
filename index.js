74078baa84817363d98bd25c8d2dd811da00a449
DarkPark <darkpark@pisem.net>
2015-12-23 00:07:54 +0200

    switch from tab to spaces

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /**
- * @module stb-util-parse-uri
- *
- * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
  * @license GNU GENERAL PUBLIC LICENSE Version 3
  */
 
@@ -18,41 +16,35 @@
  * @return {Object.<string, string>} result data
  */
 function parseUri ( str ) {
-	var o   = parseUri.options,
-		m   = o.parser[o.strictMode ? 'strict' : 'loose'].exec(str),
-		uri = {},
-		i   = 14;
+    var o   = parseUri.options,
+        m   = o.parser[o.strictMode ? 'strict' : 'loose'].exec(str),
+        uri = {},
+        i   = 14;
 
-	while ( i-- ) { uri[o.key[i]] = m[i] || ''; }
+    while ( i-- ) { uri[o.key[i]] = m[i] || ''; }
 
-	uri[o.q.name] = {};
+    uri[o.q.name] = {};
 
-	/* eslint no-unused-vars: 0 */
-	uri[o.key[12]].replace(o.q.parser, function ( $0, $1, $2 ) {
-		if ( $1 ) { uri[o.q.name][$1] = $2; }
-	});
+    /* eslint no-unused-vars: 0 */
+    uri[o.key[12]].replace(o.q.parser, function ( $0, $1, $2 ) {
+        if ( $1 ) { uri[o.q.name][$1] = $2; }
+    });
 
-	return uri;
+    return uri;
 }
 
 parseUri.options = {
-	strictMode: false,
-	key:        ['source', 'protocol', 'authority', 'userInfo', 'user', 'password', 'host', 'port', 'relative', 'path', 'directory', 'file', 'query', 'anchor'],
-	q:          {
-		name:   'queryKey',
-		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-	},
-	parser:     {
-		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-	}
+    strictMode: false,
+    key:        ['source', 'protocol', 'authority', 'userInfo', 'user', 'password', 'host', 'port', 'relative', 'path', 'directory', 'file', 'query', 'anchor'],
+    q:          {
+        name:   'queryKey',
+        parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+    },
+    parser:     {
+        strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+        loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+    }
 };
-
-
-if ( DEBUG ) {
-	// expose to the global scope
-	window.utilParseUri = parseUri;
-}
 
 
 // public
